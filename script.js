49eeb7b2a4046d0b93de80ce7f460cdd05564f48
羊 <1401612731@qq.com>
2018-02-26 21:11:48 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function clickqq()
+{
+	window.open("https://im.qq.com/index.shtml")
+}
+function clickshouye()
+{
+	window.open("hsttp://im.qq.com/")
+}
+function clickxiazai()
+{
+	window.open("https://im.qq.com/download/")
+}
+function clickdongtai()
+{
+	window.open("https://im.qq.com/news/")
+}
+function clickzc()
+{
+	window.open("https://zc.qq.com/chs/index.html")
+}
